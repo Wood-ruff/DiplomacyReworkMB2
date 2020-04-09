21a864a94e78a119abf1878d1d18c0bf1e49865d
pwald <philip.wald@gmail.com>
2020-04-09 22:49:31 +0200

    only faction leaders can now use the added button, renamed Diplomacy Reworked to DiplomacyReworked

---
+++ DiplomacyReworked/DiplomacyReworkedMenuBehaviour.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameMenus;
+using TaleWorlds.CampaignSystem.Overlay;
+using TaleWorlds.Core;
+
+namespace DiplomacyReworked
+{
+    class DiplomacyReworkedMenuBehaviour : CampaignBehaviorBase
+    {
+        private const int MENU_TOWN_INSERT_INDEX = 5;
+        private const int MENU_CASTLE_INSERT_INDEX = 3;
+        private const string MENU_TOWN_KEY = "town";
+        private const string MENU_CASTLE_KEY = "castle";
+        private const string MENU_TITLE = "Diplomacy";
+        public override void RegisterEvents()
+        {
+            CampaignEvents.OnNewGameCreatedEvent.AddNonSerializedListener(
+                 this, new Action<CampaignGameStarter>(OnAfterNewGameCreated));
+            CampaignEvents.OnGameLoadedEvent.AddNonSerializedListener(
+                this, new Action<CampaignGameStarter>(OnAfterNewGameCreated));
+        }
+
+        public void OnAfterNewGameCreated(CampaignGameStarter campaignGameStarter)
+        {
+            AddGameMenus(campaignGameStarter);
+        }
+
+        private void AddGameMenus(CampaignGameStarter campaignGameStarter)
+        {
+            campaignGameStarter.AddGameMenuOption(MENU_TOWN_KEY, "make_diplomacy_town", MENU_TITLE, new GameMenuOption.OnConditionDelegate(menuCondition), new GameMenuOption.OnConsequenceDelegate(menuConsequence), false, MENU_TOWN_INSERT_INDEX, false);
+            campaignGameStarter.AddGameMenuOption(MENU_CASTLE_KEY, "make_diplomacy_castle", MENU_TITLE, new GameMenuOption.OnConditionDelegate(menuCondition), new GameMenuOption.OnConsequenceDelegate(menuConsequence), false, MENU_CASTLE_INSERT_INDEX, false);
+        }
+
+        private void menuConsequence(MenuCallbackArgs args)
+        {
+            InformationManager.DisplayMessage(new InformationMessage("Placeholder Text"));
+        }
+
+        private bool menuCondition(MenuCallbackArgs args)
+        {
+            return Settlement.CurrentSettlement.MapFaction == Hero.MainHero.MapFaction && Hero.MainHero.IsFactionLeader;
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        {
+        }
+
+        private static void DisplayInfoMsg(string msg)
+        {
+            InformationManager.DisplayMessage(new InformationMessage(msg));
+        }
+    }
+}
