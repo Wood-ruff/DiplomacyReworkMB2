466119116359508207f0541fe12e1eab5fd40616
pwald <philip.wald@gmail.com>
2020-04-09 20:42:02 +0200

    initial commit

---
+++ Diplomacy Reworked/DiplomacyReworked.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace Diplomacy_Reworked
+{
+    public class Main
+    {
+    }
+}
