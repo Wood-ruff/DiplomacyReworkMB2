fe2539210f6ca17c7797b4ada6f787c8ecb3dfac
pwald <philip.wald@gmail.com>
2020-04-12 17:17:54 +0200

    added a Keep/pass fief feature

--- DiplomacyReworked/DiplomacyReworkedMenuBehaviour.cs
+++ DiplomacyReworked/DiplomacyReworkedMenuBehaviour.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 
 using System.Linq;
-
+using System.Collections.Generic;
 
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.Library;
 using TaleWorlds.CampaignSystem.Barterables;
 using TaleWorlds.CampaignSystem.GameMenus;
 using TaleWorlds.CampaignSystem.Overlay;
 using TaleWorlds.Core;
+using TaleWorlds.InputSystem;
+using System.Threading.Tasks;
 
 using TaleWorlds.CampaignSystem.Election;
 
@@ -40,7 +43,12 @@ namespace DiplomacyReworked
         private const string INNER_DIPLOMACY_FIEF_MENU_ID = "inner_diplomacy_fiefs";
         private const string INNER_DIPLOMACY_FIEF_MENU_TEXT = "Which Fief do you want to gift?";
 
+        private const string KEEP_FIEF_MENU_ID = "keep_fief_menu";
+        private const string KEEP_FIEF_MENU_TEXT = "Do you wish to keep the fief or let it be passed by vote?";
+  
 
+
+        private SettlementClaimantDecision currentdecision;
         private IFaction currentSelectedFaction = null;
         private Clan currentSelectedClan = null;
 
@@ -53,11 +61,42 @@ namespace DiplomacyReworked
                 this, new Action<CampaignGameStarter>(OnAfterNewGameCreated));
             CampaignEvents.KingdomDecisionConcluded.AddNonSerializedListener(this, onDecisionConcludedDelegate);
             CampaignEvents.OnSettlementOwnerChangedEvent.AddNonSerializedListener(this, settlementchangedOwnerDelegate);
+            CampaignEvents.KingdomDecisionAdded.AddNonSerializedListener(this, onDecisionAdded);
+
+        }
+
+
+        private void onDecisionAdded(KingdomDecision arg1, bool arg2)
+        {
+            if (arg1 is TaleWorlds.CampaignSystem.Election.SettlementClaimantDecision)
+            {
+                SettlementClaimantDecision decision = arg1 as SettlementClaimantDecision;
+                if (Hero.MainHero.MapFaction is Kingdom)
+                {
+                    if (decision.Kingdom == Hero.MainHero.MapFaction && decision.Settlement.OwnerClan == Hero.MainHero.Clan)
+                    {
+                        InformationManager.ShowInquiry(new InquiryData("Fief captured", KEEP_FIEF_MENU_TEXT, true, true, "Keep", "Pass",new Action(FiefKeepConfirmedAction), new Action(FiefKeepDeniedAction)));
+                        this.currentdecision = decision;
+                    }
+                }
+            }
+        }
+
+        private void FiefKeepDeniedAction()
+        {
+            Campaign.Current.GameMenuManager.MenuLocations.Clear();
+            this.currentdecision = null;
+        }
+
+        private void FiefKeepConfirmedAction()
+        {
+            Campaign.Current.RemoveDecision(this.currentdecision);
+            Campaign.Current.GameMenuManager.MenuLocations.Clear();
+            this.currentdecision = null;
         }
 
         private void settlementchangedOwnerDelegate(Settlement arg1, bool arg2, Hero arg3, Hero arg4, Hero arg5, ChangeOwnerOfSettlementAction.ChangeOwnerOfSettlementDetail arg6)
         {
-            //if(Campaign.Current.GameMenuManager.GetMenuOverlayType(new MenuContext()))
         }
 
         private void onDecisionConcludedDelegate(KingdomDecision arg1, DecisionOutcome arg2, bool arg3)
@@ -80,11 +119,6 @@ namespace DiplomacyReworked
                             if (change < 0)
                             {
                                 clan.Leader.SetPersonalRelation(Hero.MainHero, (int)clan.Leader.GetRelationWithPlayer() + (change * -1));
-                                /*if(Hero.MainHero.MapFaction.IsKingdomFaction)
-                                {
-                                    Kingdom kingdom = Hero.MainHero.MapFaction as Kingdom;
-                                    kingdom.
-                                }*/
                             }
                         }
                     }
@@ -107,14 +141,13 @@ namespace DiplomacyReworked
 
         private void AddGameMenus(CampaignGameStarter campaignGameStarter)
         {
-
             //Adding in menus and buttons for outer diplomacy
             bool isLeave = false;
             bool isRepeatable = false;
             campaignGameStarter.AddGameMenuOption(MENU_TOWN_KEY, MENU_ID + MENU_TOWN_KEY, MENU_BUTTON_TITLE, new GameMenuOption.OnConditionDelegate(menuCondition), new GameMenuOption.OnConsequenceDelegate(menuConsequence), isLeave, MENU_TOWN_INSERT_INDEX, isRepeatable);
             campaignGameStarter.AddGameMenuOption(MENU_CASTLE_KEY, MENU_ID + MENU_CASTLE_KEY, MENU_BUTTON_TITLE, new GameMenuOption.OnConditionDelegate(menuCondition), new GameMenuOption.OnConsequenceDelegate(menuConsequence), isLeave, MENU_CASTLE_INSERT_INDEX, isRepeatable);
 
-            campaignGameStarter.AddGameMenu(MENU_ID, MENU_TEXT, new OnInitDelegate(MenuOnInit), GameOverlays.MenuOverlayType.SettlementWithCharacters);
+            campaignGameStarter.AddGameMenu(MENU_ID, MENU_TEXT, new OnInitDelegate(MenuOnInit), GameOverlays.MenuOverlayType.SettlementWithBoth);
 
 
             string factionName = "";
@@ -130,11 +163,12 @@ namespace DiplomacyReworked
             }
             campaignGameStarter.AddGameMenuOption(MENU_ID, MENU_ID + "quit", "Return to Menu", alwaystrueDelegate, selectMenuQuitConsequence, isLeave, -1, isRepeatable);
 
-            campaignGameStarter.AddGameMenu(MENU_FACTION_DIPLOMACY_ID, MENU_FACTION_DIPLOMACY_TEXT, new OnInitDelegate(MenuOnInit), GameOverlays.MenuOverlayType.SettlementWithCharacters);
+
+
+            campaignGameStarter.AddGameMenu(MENU_FACTION_DIPLOMACY_ID, MENU_FACTION_DIPLOMACY_TEXT, new OnInitDelegate(MenuOnInit), GameOverlays.MenuOverlayType.SettlementWithBoth);
             campaignGameStarter.AddGameMenuOption(MENU_FACTION_DIPLOMACY_ID, MENU_FACTION_DIPLOMACY_ID + "conversation", "Enter Conversation", selectActionPeaceCondition, selectActionPeaceConsequence, isLeave, -1, isRepeatable);
             campaignGameStarter.AddGameMenuOption(MENU_FACTION_DIPLOMACY_ID, MENU_FACTION_DIPLOMACY_ID + "war", "Declare War", selectActionWarCondition, selectActionWarConsequence, isLeave, -1, isRepeatable);
-            campaignGameStarter.AddGameMenuOption(MENU_FACTION_DIPLOMACY_ID, MENU_FACTION_DIPLOMACY_ID + "quit", "Return to Selection", alwaystrueDelegate, selectActionQuitConsequence, isLeave, -1, isRepeatable);
-
+            campaignGameStarter.AddGameMenuOption(MENU_FACTION_DIPLOMACY_ID, MENU_FACTION_DIPLOMACY_ID + "quit", "Return to Selection", alwaystrueDelegate, selectActionQuitConsequence, true, -1, isRepeatable);
 
 
 
@@ -142,7 +176,7 @@ namespace DiplomacyReworked
             //Adding in menus and buttons for inner diplomacy
             campaignGameStarter.AddGameMenuOption(MENU_TOWN_KEY, INNER_DIPLOMACY_OPTION_ID, MENU_INNER_DIPLOMACY_TEXT, innerDiplomacyCondition, innerDiplomacyConsequence, isLeave, MENU_TOWN_INSERT_INDEX + 1, isRepeatable);
             campaignGameStarter.AddGameMenuOption(MENU_CASTLE_KEY, INNER_DIPLOMACY_OPTION_ID, MENU_INNER_DIPLOMACY_TEXT, innerDiplomacyCondition, innerDiplomacyConsequence, isLeave, MENU_CASTLE_INSERT_INDEX + 1, isRepeatable);
-            campaignGameStarter.AddGameMenu(MENU_INNER_DIPLOMACY_ID, MENU_INNER_DIPLOMACY_TITLE, MenuOnInit, GameOverlays.MenuOverlayType.SettlementWithCharacters);
+            campaignGameStarter.AddGameMenu(MENU_INNER_DIPLOMACY_ID, MENU_INNER_DIPLOMACY_TITLE, MenuOnInit, GameOverlays.MenuOverlayType.SettlementWithBoth);
             if (Hero.MainHero.MapFaction is Kingdom)
             {
                 foreach (Clan clan in (Hero.MainHero.MapFaction as Kingdom).Clans)
@@ -153,28 +187,42 @@ namespace DiplomacyReworked
                     }
                 }
             }
-            campaignGameStarter.AddGameMenuOption(MENU_INNER_DIPLOMACY_ID, MENU_INNER_DIPLOMACY_ID + "quit", "Return to Menu", alwaystrueDelegate, selectMenuQuitConsequence, isLeave, -1, isRepeatable);
+            campaignGameStarter.AddGameMenuOption(MENU_INNER_DIPLOMACY_ID, MENU_INNER_DIPLOMACY_ID + "quit", "Return to Menu", alwaystrueDelegate, selectMenuQuitConsequence, true, -1, isRepeatable);
 
 
-            campaignGameStarter.AddGameMenu(INNER_DIPLOMACY_CLAN_MENU_ID, INNER_DIPLOMACY_CLAN_MENU_TITLE, MenuOnInit, GameOverlays.MenuOverlayType.SettlementWithCharacters);
+            campaignGameStarter.AddGameMenu(INNER_DIPLOMACY_CLAN_MENU_ID, INNER_DIPLOMACY_CLAN_MENU_TITLE, MenuOnInit, GameOverlays.MenuOverlayType.SettlementWithBoth);
             campaignGameStarter.AddGameMenuOption(INNER_DIPLOMACY_CLAN_MENU_ID, INNER_DIPLOMACY_CLAN_MENU_ID + "giftFief", "Gift this clan a Fief", giftFiefCondition, giftFiefConsequence, isLeave, -1, isRepeatable);
             campaignGameStarter.AddGameMenuOption(INNER_DIPLOMACY_CLAN_MENU_ID, INNER_DIPLOMACY_CLAN_MENU_ID + "conversation", "Enter Conversation with the Leader", talkToLeaderCondition, talkToLeaderConsquence, isLeave, -1, isRepeatable);
-            campaignGameStarter.AddGameMenuOption(INNER_DIPLOMACY_CLAN_MENU_ID, INNER_DIPLOMACY_CLAN_MENU_ID + "quit", "Return to Clans", alwaystrueDelegate, innerDiplomacyClanQuitConsequence, isLeave, -1, isRepeatable);
+            campaignGameStarter.AddGameMenuOption(INNER_DIPLOMACY_CLAN_MENU_ID, INNER_DIPLOMACY_CLAN_MENU_ID + "quit", "Return to Clans", alwaystrueDelegate, innerDiplomacyClanQuitConsequence, true, -1, isRepeatable);
 
 
-            campaignGameStarter.AddGameMenu(INNER_DIPLOMACY_FIEF_MENU_ID, INNER_DIPLOMACY_FIEF_MENU_TEXT, MenuOnInit, GameOverlays.MenuOverlayType.SettlementWithCharacters);
-            
-            foreach(Settlement settlement in Settlement.All)
+            campaignGameStarter.AddGameMenu(INNER_DIPLOMACY_FIEF_MENU_ID, INNER_DIPLOMACY_FIEF_MENU_TEXT, MenuOnInit, GameOverlays.MenuOverlayType.SettlementWithBoth);
+
+            foreach (Settlement settlement in Settlement.All)
             {
-                if(settlement.IsCastle || settlement.IsTown)
+                if (settlement.IsCastle || settlement.IsTown)
                 {
 
-                campaignGameStarter.AddGameMenuOption(INNER_DIPLOMACY_FIEF_MENU_ID, INNER_DIPLOMACY_FIEF_MENU_ID + settlement.Name.ToString(), settlement.Name.ToString(), isSettlementPlayerOwnedDelegate, settlementGiftedConsequence, isLeave, -1, isRepeatable);
+                    campaignGameStarter.AddGameMenuOption(INNER_DIPLOMACY_FIEF_MENU_ID, INNER_DIPLOMACY_FIEF_MENU_ID + settlement.Name.ToString(), settlement.Name.ToString(), isSettlementPlayerOwnedDelegate, settlementGiftedConsequence, isLeave, -1, isRepeatable);
                 }
             }
 
             campaignGameStarter.AddGameMenuOption(INNER_DIPLOMACY_FIEF_MENU_ID, INNER_DIPLOMACY_FIEF_MENU_ID + "quit", "Return to Options", alwaystrueDelegate, innerDiplomacyFiefQuitConsequence, isLeave, -1, isRepeatable);
+
+
         }
+
+        /*
+        private void PassFiefConsequence(MenuCallbackArgs args)
+        {
+            Campaign.Current.GameMenuManager.MenuLocations.Clear();
+        }
+
+        private void KeepFiefConsequence(MenuCallbackArgs args)
+        {
+            Campaign.Current.RemoveDecision(this.currentdecision);
+            Campaign.Current.GameMenuManager.MenuLocations.Clear();
+        }*/
 
         private void talkToLeaderConsquence(MenuCallbackArgs args)
         {
@@ -197,6 +245,42 @@ namespace DiplomacyReworked
 
         private void settlementGiftedConsequence(MenuCallbackArgs args)
         {
+            try
+            {
+
+                Settlement found = null;
+                foreach (Settlement settlement in Settlement.All)
+                {
+                    if (settlement.Name.ToString() == args.Text.ToString())
+                    {
+                        found = settlement;
+                        break;
+                    }
+                }
+                if (found == null) { DisplayInfoMsg("Error, could not find chosen fief"); return; }
+                // FiefBarterable giftedFief = new FiefBarterable(found, found.OwnerClan.Leader, this.currentSelectedClan.Leader);
+
+                FiefBarterable giftedFief = new FiefBarterable(found, found.OwnerClan.Leader, found.OwnerClan.Leader.OwnedParties.First(), found.OwnerClan.Leader);
+
+                giftedFief.Apply();
+                if (found.IsCastle)
+                {
+                    currentSelectedClan.Leader.SetPersonalRelation(Hero.MainHero, (int)currentSelectedClan.Leader.GetRelationWithPlayer() + 30);
+                }
+                else
+                {
+                    currentSelectedClan.Leader.SetPersonalRelation(Hero.MainHero, (int)currentSelectedClan.Leader.GetRelationWithPlayer() + 50);
+                }
+                GameMenu.SwitchToMenu(INNER_DIPLOMACY_CLAN_MENU_ID);
+            }
+            catch (Exception e)
+            {
+                DisplayInfoMsg("DiplomacyReworked:An Error occurred when gifting a Fief");
+            }
+        }
+
+        private bool isSettlementPlayerOwnedDelegate(MenuCallbackArgs args)
+        {
             Settlement found = null;
             foreach (Settlement settlement in Settlement.All)
             {
@@ -205,31 +289,6 @@ namespace DiplomacyReworked
                     found = settlement;
                     break;
                 }
-            }
-            if (found == null) { DisplayInfoMsg("Error, could not find chosen fief"); return; }
-            FiefBarterable giftedFief = new FiefBarterable(found,found.OwnerClan.Leader,found.OwnerClan.Leader.OwnedParties.First(),currentSelectedClan.Leader);
-            giftedFief.Apply();
-            if (found.IsCastle)
-            {
-                currentSelectedClan.Leader.SetPersonalRelation(Hero.MainHero,(int) currentSelectedClan.Leader.GetRelationWithPlayer() + 30);
-            }
-            else
-            {
-                currentSelectedClan.Leader.SetPersonalRelation(Hero.MainHero, (int)currentSelectedClan.Leader.GetRelationWithPlayer() + 50);
-            }
-            GameMenu.SwitchToMenu(INNER_DIPLOMACY_CLAN_MENU_ID);
-        }
-
-        private bool isSettlementPlayerOwnedDelegate(MenuCallbackArgs args)
-        {
-            Settlement found = null;
-            foreach(Settlement settlement in Settlement.All)
-            {
-                if(settlement.Name.ToString() == args.Text.ToString())
-                {
-                    found = settlement;
-                    break;
-                } 
             }
             if (found == null) return false;
             return found.OwnerClan.Name.ToString() == Hero.MainHero.Clan.Name.ToString();
@@ -260,7 +319,7 @@ namespace DiplomacyReworked
                     this.currentSelectedClan = clan;
                 }
             }
-            if(currentSelectedClan != null)
+            if (currentSelectedClan != null)
             {
                 GameMenu.SwitchToMenu(INNER_DIPLOMACY_CLAN_MENU_ID);
             }
@@ -411,10 +470,6 @@ namespace DiplomacyReworked
             return Settlement.CurrentSettlement.MapFaction == Hero.MainHero.MapFaction && Hero.MainHero.IsFactionLeader;
         }
 
-        public override void SyncData(IDataStore dataStore)
-        {
-        }
-
         private static void DisplayInfoMsg(string msg)
         {
             InformationManager.DisplayMessage(new InformationMessage(msg));
@@ -432,6 +487,10 @@ namespace DiplomacyReworked
                 DisplayInfoMsg("An Error occurred when initializing the conversation");
                 logString(e.ToString());
             }
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        {
         }
     }
 }
